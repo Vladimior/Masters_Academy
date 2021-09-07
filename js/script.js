0c801f03fe5bea2205b03432c4fdda428addc0db
volodymyr_khrusch <vladimir.khrush@gmail.com>
2021-09-07 14:10:40 +0300

    Optimized code, added new styles

--- js/script.js
+++ js/script.js
@@ -1,65 +1,55 @@
 // start coordinates
-const pandemicStartMap = "XX0X10010X000X010X0";
+const pandemicStartMap = 'XX0X10010X000X010X0';
+
+// Parametrs
+const infected = 'red';
+const uninfected = 'green';
+const ocean = 'blue';
+const classNameInfected = 'partRed';
+const classNameUninfected = 'partGreen';
+const classNameOcean = 'partBlue';
+const pandemicStartId = 'pandemicStart';
+const pandemicEndId = 'pandemicEnd';
 
 // square creation function
-function createDiv(className, color){
-    var div = document.createElement('div');
-    div.className    = className;
-    div.style.width  = '40px';
-    div.style.height = '40px';
-    div.style.border = '1px solid grey';
-    div.style.marginLeft = '4px';
-    div.style.backgroundColor = color;
-    return div;
-  };
-
-  // start map creation function
-function CreateMap(coordinates){
-    for (var i = 0; i < coordinates.length; i++) {
-        if (coordinates[i] == 'X'){
-          document.getElementById("pandemicStart").appendChild(createDiv('partBlue', 'blue'));
-          
-        }
-        else if (coordinates[i] == '0'){
-            document.getElementById("pandemicStart").appendChild(createDiv('partGreen', 'green'));
-        }
-        else if (coordinates[i] == '1'){
-            document.getElementById("pandemicStart").appendChild(createDiv('partRed', 'red'));
-        }
-      }
+function createDiv(className, color) {
+  var div = document.createElement('div');
+  div.className = className;
+  div.style.backgroundColor = color;
+  return div;
 };
 
-// final map creation function
-function CreateMapEnd(coordinates){
+//map creation function
+function сreateMap(coordinates, elementId) {
   for (var i = 0; i < coordinates.length; i++) {
-      if (coordinates[i] == 'X'){
-        document.getElementById("pandemicEnd").appendChild(createDiv('partBlue', 'blue'));
-        
-      }
-      else if (coordinates[i] == '0'){
-          document.getElementById("pandemicEnd").appendChild(createDiv('partGreen', 'green'));
-      }
-      else if (coordinates[i] == '1'){
-          document.getElementById("pandemicEnd").appendChild(createDiv('partRed', 'red'));
-      }
+    if (coordinates[i] === 'X') {
+      document.getElementById(elementId).appendChild(createDiv(classNameOcean, ocean));
     }
+    else if (coordinates[i] === '0') {
+      document.getElementById(elementId).appendChild(createDiv(classNameUninfected, uninfected));
+    }
+    else if (coordinates[i] === '1') {
+      document.getElementById(elementId).appendChild(createDiv(classNameInfected, infected));
+    }
+  }
 };
 
 // translate to array, find and replace, translate to string
 let pandemicENDtMap = pandemicStartMap.split('X').map(end => end.includes('1') ? end.replace(/0/g, '1') : end).join('X');
 
 // calculation of indicators
+let total = pandemicENDtMap.replace(/X/g, '').length;
+let infection = pandemicENDtMap.split('1').length - 1;
+let percent = (infection * 100) / total;
 
-var total =  pandemicENDtMap.replace(/X/g, '').length;
-var infection = pandemicENDtMap.split('1').length - 1;
-var percent = (infection * 100) / total;
+// output of results
+document.getElementById('total').innerHTML = 'Total: ' + total;
+document.getElementById('infected').innerHTML = 'Infected: ' + infection;
+document.getElementById('percentage').innerHTML = 'Percentage: ' + percent + '%';
+document.getElementById('startCoordinat').innerHTML = pandemicStartMap;
 
-
-document.getElementById("total").innerHTML = "Total: " + total;
-document.getElementById("infected").innerHTML = "Infected: " + infection ;
-document.getElementById("percentage").innerHTML = "Percentage: " + percent +'%'
-
-CreateMap(pandemicStartMap)
-CreateMapEnd(pandemicENDtMap)
+// map creation
+сreateMap(pandemicStartMap, pandemicStartId);
+сreateMap(pandemicENDtMap, pandemicEndId);
 
 
